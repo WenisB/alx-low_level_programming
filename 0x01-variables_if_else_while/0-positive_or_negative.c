02b8965fa58b1f9c3605d26e4d2b9a056bdab221
Wenis Bulus <buluswenis@gmail.com>
2022-12-12 07:56:12 -0800

    Positive or negative correctios 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,23 +7,18 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	/*My code starts here*/
-	
-	if (n>0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else 
 		printf("%d is negative\n", n);
-	/*End of my code*/
 
 	return (0);
 }
