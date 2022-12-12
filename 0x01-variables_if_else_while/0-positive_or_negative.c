22c68ff1d192105b5d2f448ac033f1f493180916
Wenis Bulus <buluswenis@gmail.com>
2022-12-12 07:27:33 -0800

    Positive or negative correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,28 @@
-#include<stdio.h>
-#include<time.h>
 #include<stdlib.h>
-/*more headers goes there*/
+#include<time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
- 
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n=rand()-RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
+
+	/*My code starts here*/
 	
-	/*my code goes here right?*/
 	if (n>0)
 		printf("%d is positive\n", n);
-	if (n==0)
+	else if (n==0)
 		printf("%d is zero\n", n);
-	if (n<0)
-		printf("%d is negative\n");
+	else 
+		printf("%d is negative\n", n);
+	/*End of my code*/
 
 	return (0);
 }
