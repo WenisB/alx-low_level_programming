9c49ddce66983b88c9cc96bc9678d777f165000c
Wenis Bulus <buluswenis@gmail.com>
2022-12-13 09:46:30 -0800

    task 1: Printing lower case lettes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * Prints the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
