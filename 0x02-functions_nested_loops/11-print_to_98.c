caba310366158da9e9be9f61b641450272d34d12
Wenis Bulus <buluswenis@gmail.com>
2022-12-14 10:02:56 -0800

    task 11: Printing to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - prints to 98
+ * @n: The offset value to start from
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
