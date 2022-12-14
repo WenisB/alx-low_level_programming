61f5ef321e0f7a6e9c1a2cab624b691dff57c20e
Wenis Bulus <buluswenis@gmail.com>
2022-12-14 08:46:07 -0800

    Nine times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints out the 9 times table
+ */
+
+void times_table(void)
+{
+	int num, mul, result;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mul = 0; mul <= 9; mul++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			result = num * mul;
+
+			if (result < 9)
+				_putchar(' ');
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
