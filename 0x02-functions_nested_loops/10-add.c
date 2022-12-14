73b15c03c35ada3c2c8ba81fb1b84462fff38582
Wenis Bulus <buluswenis@gmail.com>
2022-12-14 09:39:43 -0800

    Task 10: Adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *@x: the first argument
+ *@y: the second argument
+ *
+ * Return: The result of adding to integers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
