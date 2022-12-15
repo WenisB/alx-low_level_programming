65b7c8b647e55c1256ac676d1589af1932294550
Wenis Bulus <buluswenis@gmail.com>
2022-12-15 08:00:22 -0800

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,10 +6,9 @@
 
 #include <stdio.h>
 
-
-
+void print_remaining_days(int month, int day, int year)
+nt convert_day(int month, int day);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-
-
+void print_remaining_days(int month, int day, int year);
 #endif /* MAIN_H */
