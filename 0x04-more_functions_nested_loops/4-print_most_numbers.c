84aa3929e7b05f851501a0bda4c93fbc475a8b7a
Wenis Bulus <buluswenis@gmail.com>
2022-12-15 10:55:03 -0800

    Task 4 complete

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers between 0-9
+ * except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void);
+{
+	int c;
+
+	for (c = 48; c <= 58; c++)
+	{
+		if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
+
