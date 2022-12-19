8c40c517c23a72560bed52775e970d648e104295
Wenis Bulus <buluswenis@gmail.com>
2022-12-19 04:39:12 -0800

    Task 3: printing string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - puts in charactres
+ * @str: string to put
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*Str++);
+	}
+	_putchar('\n');
+}
