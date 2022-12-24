66def802e276c41736db685e305f49c20fe9f39e
Wenis Bulus <buluswenis@gmail.com>
2022-12-23 20:40:53 -0800

    task 0: compleat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest:destination
+ * @src: source
+ *
+ * Return: concatinatedstring
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (inde = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
