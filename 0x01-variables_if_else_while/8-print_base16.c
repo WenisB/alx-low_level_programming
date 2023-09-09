2da95a7ebe5cebbd9cc8b20d47c8ff4b6a9e9e78
Wenis Bulus <buluswenis@gmail.com>
2023-09-09 18:18:45 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point Prints alphabets first lower then UPPER CASE
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 0; num < 10; num++)
+		putchar((num) + '0');
+	for (alpha = 'A'; alpha <= 'F'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
