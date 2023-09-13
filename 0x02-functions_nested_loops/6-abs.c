335eef25b84476d94a5714b6a0c210cb27ea6166
Wenis Bulus <buluswenis@gmail.com>
2023-09-13 10:14:50 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolut value of an input number
+ *@x: value to inspect
+ * Return: Always positive integer
+ */
+int _abs(int x)
+{
+	if (x > 0)
+		return (x);
+	else
+		return (-x);
+}
