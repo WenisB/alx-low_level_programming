c11ad22099f979ccbcc0bec6608c0fe5854ffe1a
Wenis Bulus <buluswenis@gmail.com>
2023-09-13 08:12:08 +0000

    Islower or Not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * islower - Prints weather a letter is lowercase or not
+ * _islower - Prints weather a letter is lowercase or not
+ * @c: Character to check
  *
  * Return: Always 1 (Success);
  */
 
-int _islower (int c)
+int _islower(int c)
 {
 	if ((c >= 'a') && (c <= 'z'))
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
