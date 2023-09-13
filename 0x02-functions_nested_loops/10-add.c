708c90c3c938d76fb4cee35023bfaaa46599bfcd
Wenis Bulus <buluswenis@gmail.com>
2023-09-13 14:17:07 +0000

    9 times tableclear

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two intergers and returns the result
+ *@a: first value
+ *@b: Second value
+ *
+ * Return: interger (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
