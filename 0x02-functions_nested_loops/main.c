32daf4bb01e50a39ab6f5eda5541935ae9a63335
Wenis Bulus <buluswenis@gmail.com>
2023-09-13 12:53:48 +0000

    Jack Bauer

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * main - check the code
  *
@@ -7,12 +6,6 @@
  */
 int main(void)
 {
-	int r;
-
-	 print_last_digit(98);
-	     print_last_digit(0);
-	         r = print_last_digit(-1024);
-		     _putchar('0' + r);
-		         _putchar('\n');
-			     return (0);
+	jack_bauer();
+	return (0);
 }
