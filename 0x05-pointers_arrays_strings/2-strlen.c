6e1b5fd76446e82e8aedd91b448a63abd025210e
Wenis Bulus <buluswenis@gmail.com>
2023-09-18 16:17:30 +0000

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,18 @@
 /**
  * _strlen - Function that returns the length of a string
  *
- * @s: the input string
- *
+ * @s: the input st
  * Return: Length of string
  */
 int _strlen(char *s)
 {
-	int index;
+	int i;
 
-	for (index = 0; s[index] != '\0' ; index++)
-		return (index);
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
