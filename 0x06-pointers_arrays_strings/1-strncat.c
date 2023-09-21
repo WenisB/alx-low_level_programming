48ad0d70bbf7ba301c29ab27cde5a363db2c862f
Wenis Bulus <buluswenis@gmail.com>
2023-09-21 10:58:44 +0000

    Task 1: String cat 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *_strncat - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int lgthd, lgths;
+
+	lgthd = 0;
+	lgths = 0;
+
+	while (*(dest + lgthd) != '\0')
+		lgths++;
+
+	while (*(src + lgths) != '\0' && lgthd < 97 && lgths < n)
+	{
+		*(dest + lgthd) = *(src + lgths);
+		lgthd++;
+		lgths++;
+	}
+	*(dest + lgthd) = '\0';
+	return (dest);
+}
