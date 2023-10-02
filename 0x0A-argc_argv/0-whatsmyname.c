d6989b10e1753a04abcb9563f585823b55869bed
Wenis Bulus <buluswenis@gmail.com>
2023-10-02 15:58:14 +0000

    Task 0 again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-int main (int argc, char *argv[])
+/**
+ * main - prints the name of program
+ * @argc: First argument
+ * @argv: Second argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
